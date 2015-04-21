ac15a05d5c9d66ed202e6a78b62f8b56b6e1e1b4
Justina Chen <rcfans@163.com>
2015-04-21 08:43:26 +0800

    new entry name

--- easy-selenium.py
+++ easy-selenium.py
@@ -81,34 +81,34 @@ class WebTestCase(unittest.TestCase):
     def testChrome(self):
         WebTestCase.wait_seconds = 30
         WebTestCase.desired_cap = desired_cap = {'browser': 'Chrome', 'browser_version': '32.0', 'os': 'OS X', 'os_version': 'Lion', 'resolution': '1024x768'}
-        self.quote()
+        self.start()
 
     def testFireFox(self):
         WebTestCase.wait_seconds = 30
         WebTestCase.desired_cap = {'browser': 'Firefox', 'browser_version': '26.0', 'os': 'OS X', 'os_version': 'Lion', 'resolution': '1024x768'}
-        self.quote()
+        self.start()
 
     def testIE8(self):
         WebTestCase.wait_seconds = 40
         WebTestCase.desired_cap = {'browser': 'IE', 'browser_version': '8.0', 'os': 'Windows', 'os_version': '7', 'resolution': '1024x768'}
-        self.quote()
+        self.start()
 
     def testIE10(self):
         WebTestCase.wait_seconds = 40
         WebTestCase.desired_cap = {'browser': 'IE', 'browser_version': '10.0', 'os': 'Windows', 'os_version': '7', 'resolution': '1024x768'}
-        self.quote()
+        self.start()
 
     def testSafari(self):
         WebTestCase.wait_seconds = 30
         WebTestCase.desired_cap = {'browser': 'Safari', 'browser_version': '7.0', 'os': 'OS X', 'os_version': 'Mavericks', 'resolution': '1024x768'}
-        self.quote()
+        self.start()
 
     def testAndroid(self):
         WebTestCase.wait_seconds = 60
         WebTestCase.desired_cap = {'browserName': 'android', 'platform': 'ANDROID', 'device': 'Samsung Galaxy S III'}
-        self.quote()
+        self.start()
 
     def testiPhone(self):
         WebTestCase.wait_seconds = 20
         WebTestCase.desired_cap = {'browserName': 'iPhone', 'platform': 'MAC', 'device': 'iPhone 5'}
-        self.quote()
+        self.start()
